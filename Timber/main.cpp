c869b38c6e430359481aa0f0a340884e65e93b47
Karol Michonski <karolmichonsky@gmail.com>
2023-10-16 21:47:31 +0200

    Dodaj pliki projektów.

---
+++ Timber/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+
+int main(void) {
+	std::cout << "Hello from SFML" << "\n";
+
+	system("pause");
+	return 0;
+}
