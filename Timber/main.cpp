d3af2e3bb16d421d0c7a24e71ffe5443eb36bd44
Karol Michonski <karolmichonsky@gmail.com>
2023-10-17 22:46:37 +0200

    Change queue to vector and fix bugs

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <queue>
+#include <vector>
 #include <conio.h>
 #include <ctime>
 #include <time.h> 
@@ -11,27 +11,29 @@
 
 using namespace std;
 
-void branchUpdate(queue<int> branches) {
+void updateBranches(vector<int> branches) {
 	srand(time(0));
 	int random = (rand() % 3);
-	branches.pop();
-	branches.push(random);
+	branches.pop_back();
+	branches.insert(branches.begin(), random);
 }
 
-void displayBranches(queue<int> branches) {
+void displayBranches(vector<int> branches) {
 	system("cls");
-	queue<int> displayBranches(branches);
+	vector<int> branchesCopy;
+	for (int i = 0; i < branches.size(); i++)
+		branchesCopy.push_back(branches[i]);
+
 	for (int i = 0; i < 5; i++) {
-		if (displayBranches.front() == 0) {
+		if (branchesCopy[i] == 0) {
 			cout << "--[   ]  " << endl;
 		}
-		else if (displayBranches.front() == 1) {
+		else if (branchesCopy[i] == 1) {
 			cout << "  [   ]--  " << endl;
 		}
 		else {
 			cout << "  [   ]" << endl;
 		}
-		displayBranches.pop();
 	}
 	
 }
@@ -40,39 +42,37 @@ int main(void) {
 	
 	sf::Window window(sf::VideoMode(800, 600), "TimberTime");
 	srand(time(0));
-	queue<int> branches;
+	vector<int> branches;
 	
 	for (int i = 0; i < 5; i++) {
-		
 		int random = (rand() % 3);
-		cout << random;
-		branches.push(random);
+		branches.push_back(random);
 	}
 	displayBranches(branches);
-
 	while (window.isOpen()) {
 		sf::Event event;
-		displayBranches(branches);
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 			if (event.type == sf::Event::KeyPressed) {
+				int random = (rand() % 3);
 				if (event.key.code == sf::Keyboard::A) {
-					if (branches.front() == 0) {
-						window.close();
-					}	
-					int random = (rand() % 3);
-					branches.pop();
-					branches.push(random);
-				}
-				else if (event.key.code == sf::Keyboard::D) {
-					if (branches.front() == 1) {
+					if (branches[4] == 0) {
 						window.close();
 					}
-					int random = (rand() % 3);
-					branches.pop();
-					branches.push(random);
+					branches.pop_back();
+					branches.insert(branches.begin(), random);
+					displayBranches(branches);
+
+				}
+				else if (event.key.code == sf::Keyboard::D) {
+					if (branches[4] == 1) {
+						window.close();
+					}
+					branches.pop_back();
+					branches.insert(branches.begin(), random);
+					displayBranches(branches);
 				}
 			}		
 		}
