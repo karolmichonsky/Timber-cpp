43cbc24b71a9273873981b0807c6f6c34e228216
Karol Michonski <karolmichonsky@gmail.com>
2023-11-17 23:09:42 +0100

    Create leaderboard class

---
+++ Timber/Leaderboard.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp> 
+
+using namespace std;
+
+class Leaderboard
+{
+private:
+
+public:
+
+};
+
